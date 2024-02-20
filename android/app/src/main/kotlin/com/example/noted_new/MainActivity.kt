35aaecd255c64774a649d6ccbd7a0198175639d6
ahmadelbaz <ahmeed.elbaz@gmail.com>
2024-02-20 21:00:39 +0200

    Created the project with all Clean Architecture folders, And finished app class & file

---
+++ android/app/src/main/kotlin/com/example/noted_new/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.noted_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:noted_new/app/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
