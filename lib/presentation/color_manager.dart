60ce9be940afc9428f3841d9814caf2b14a11860
ahmadelbaz <ahmeed.elbaz@gmail.com>
2024-02-26 03:44:07 +0200

    Added color manager with some base colors

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primary = Color(0xFF662549);
+  static const Color darkPrimary = Color(0xFF451952);
+  static const Color light = Color(0xFFF39F5A);
+  static const Color lightBackground = Color(0xFFE8BCB9);
+  static const Color dark = Color(0xFF1D1A39);
+  static const Color grey = Color(0xFF9098B1);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color red = Colors.red;
+}
