f51c2ebd154c4a2ffb3f15477491b203aae67ccb
ahmadelbaz <ahmeed.elbaz@gmail.com>
2024-03-22 02:58:56 +0200

    Add AppSizes class with predefined size constants
    Defines a new class AppSizes with predefined double size constants for use in
    the application's presentation layer. Sizes range from 4.0 to 200.0.

---
+++ lib/presentation/value_manager.dart
@@ -0,0 +1,14 @@
+class AppSizes {
+  static const double s4 = 4.0;
+  static const double s8 = 8.0;
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+  static const double s22 = 22.0;
+  static const double s28 = 28.0;
+  static const double s50 = 50.0;
+  static const double s100 = 100.0;
+  static const double s200 = 200.0;
+}
